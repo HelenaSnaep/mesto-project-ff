5264a4801554328b696a40b03cbeb2dff02d24e5
Kolosova Elena <HelenaSnaep@yandex.ru>
2025-02-03 02:00:58 +0400

    add functionality

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
+https://github.com/HelenaSnaep/mesto-project-ff
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,30 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document
+  .querySelector("#card-template")
+  .content.querySelector(".card");
 // @todo: DOM узлы
-
+const placesList = document.querySelector(".places__list");
 // @todo: Функция создания карточки
+function createCard(cardData, onDeleteCard) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
 
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.alt;
+
+  cardDeleteButton.addEventListener("click", onDeleteCard);
+
+  return cardElement;
+}
 // @todo: Функция удаления карточки
-
+function onDeleteCard(event) {
+  const cardElement = event.target.closest(".card");
+  cardElement.remove();
+}
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(cardData) {
+    placesList.append(createCard(cardData, onDeleteCard));
+})
